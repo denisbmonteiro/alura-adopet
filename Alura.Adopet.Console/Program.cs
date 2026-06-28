89bd77116b2ee7c8cf1fb92df175181818b24467
Denis Bittencourt <denis765@gmail.com>
2026-06-27 23:52:01 -0300

    Aplicando refatorações da aula 1 tópico 8

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -7,14 +7,13 @@ HttpClient client = ConfiguraHttpClient("http://localhost:5057");
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
-    // args[0] é o comando a ser executado pelo programa
-    switch (args[0].Trim())
+    string comandoParaExecutar = args[0].Trim();
+    switch (comandoParaExecutar)
     {
         case "import":
             List<Pet> listaDePet = new List<Pet>();
-
-            // args[1] é o caminho do arquivo a ser importado
-            using (StreamReader sr = new StreamReader(args[1]))
+            string caminhoDoArquivoDeImportacao = args[1];
+            using (StreamReader sr = new StreamReader(caminhoDoArquivoDeImportacao))
             {
                 while (!sr.EndOfStream)
                 {
@@ -60,21 +59,27 @@ try
             // exibe o help daquele comando específico
             else if (args.Length == 2)
             {
-                if (args[1].Equals("import"))
+                string comandoASerExibido = args[1];
+                if (comandoASerExibido.Equals("import"))
                 {
                     Console.WriteLine(" adopet import <arquivo> " +
                         "comando que realiza a importação do arquivo de pets.");
                 }
-                if (args[1].Equals("show"))
+                if (comandoASerExibido.Equals("show"))
                 {
                     Console.WriteLine(" adopet show <arquivo>  comando que " +
                         "exibe no terminal o conteúdo do arquivo importado.");
                 }
+                if (comandoASerExibido.Equals("list"))
+                {
+                    Console.WriteLine(" adopet list  comando que " +
+                        "exibe no terminal a lista de pets cadastrados na API.");
+                }
             }
             break;
         case "show":
-            // args[1] é o caminho do arquivo a ser exibido
-            using (StreamReader sr = new StreamReader(args[1]))
+            string caminhoDoArquivoASerExibido = args[1];
+            using (StreamReader sr = new StreamReader(caminhoDoArquivoASerExibido))
             {
                 Console.WriteLine("----- Serão importados os dados abaixo -----");
                 while (!sr.EndOfStream)
