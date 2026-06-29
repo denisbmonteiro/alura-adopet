1de2cb630e5aeb4a8ebf6d09590b57bc6c841f7c
Denis Bittencourt <denis765@gmail.com>
2026-06-29 01:47:56 -0300

    Aplicando refatorações da aula 2 tópico 3

--- Alura.Adopet.Console/List.cs
+++ Alura.Adopet.Console/List.cs
@@ -1,44 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Alura.Adopet.Console
+namespace Alura.Adopet.Console;
+
+internal class List
 {
-    internal class List
+    HttpClient client;
+    public List()
     {
-        HttpClient client;
-        public List()
-        {
-            client = ConfiguraHttpClient("http://localhost:5057");
-        }
-        public async Task ListaDadosPetsDaAPIAsync()
-        {
-            IEnumerable<Pet>? pets = await ListPetsAsync();
-            System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
-            foreach (var pet in pets)
-            {
-                System.Console.WriteLine(pet);
-            }
-        }
+        client = ConfiguraHttpClient("http://localhost:5057");
+    }
 
-        HttpClient ConfiguraHttpClient(string url)
+    public async Task ListaDadosPetsDaAPIAsync()
+    {
+        IEnumerable<Pet>? pets = await ListPetsAsync();
+        System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
+        foreach (var pet in pets)
         {
-            HttpClient _client = new HttpClient();
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            _client.BaseAddress = new Uri(url);
-            return _client;
+            System.Console.WriteLine(pet);
         }
+    }
 
-        async Task<IEnumerable<Pet>?> ListPetsAsync()
-        {
-            HttpResponseMessage response = await client.GetAsync("pet/list");
-            return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
-        }
+    HttpClient ConfiguraHttpClient(string url)
+    {
+        HttpClient _client = new HttpClient();
+        _client.DefaultRequestHeaders.Accept.Clear();
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        _client.BaseAddress = new Uri(url);
+        return _client;
+    }
+
+    async Task<IEnumerable<Pet>?> ListPetsAsync()
+    {
+        HttpResponseMessage response = await client.GetAsync("pet/list");
+        return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
     }
 }
