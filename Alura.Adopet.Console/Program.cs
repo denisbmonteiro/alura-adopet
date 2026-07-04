2e248a06bf63241cbc74de1797621e529837e95e
Denis Bittencourt <denis765@gmail.com>
2026-07-03 23:33:20 -0300

    Aplicando refatorações na classe Program

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -1,11 +1,7 @@
-﻿using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using Alura.Adopet.Console.Comandos;
-using Alura.Adopet.Console.Modelos;
+﻿using Alura.Adopet.Console.Comandos;
 
-// na linha abaixo cria-se uma instância de HttpClient para consumir API Adopet.
-HttpClient client = ConfiguraHttpClient("http://localhost:5057");
 Console.ForegroundColor = ConsoleColor.Green;
+
 try
 {
     string comando = args[0].Trim();
@@ -34,36 +30,10 @@ try
 }
 catch (Exception ex)
 {
-    // mostra a exceção em vermelho
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"Aconteceu um exceção: {ex.Message}");
 }
 finally
 {
     Console.ForegroundColor = ConsoleColor.White;
-}
-
-HttpClient ConfiguraHttpClient(string url)
-{
-    HttpClient _client = new HttpClient();
-    _client.DefaultRequestHeaders.Accept.Clear();
-    _client.DefaultRequestHeaders.Accept.Add(
-        new MediaTypeWithQualityHeaderValue("application/json"));
-    _client.BaseAddress = new Uri(url);
-    return _client;
-}
-
-Task<HttpResponseMessage> CreatePetAsync(Pet pet)
-{
-    HttpResponseMessage? response = null;
-    using (response = new HttpResponseMessage())
-    {
-        return client.PostAsJsonAsync("pet/add", pet);
-    }
-}
-
-async Task<IEnumerable<Pet>?> ListPetsAsync()
-{
-    HttpResponseMessage response = await client.GetAsync("pet/list");
-    return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
 }
