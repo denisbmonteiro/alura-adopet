bfcafa3bdf726ed86a95a1e682c3824bdcd7a546
Denis Bittencourt <denis765@gmail.com>
2026-07-03 23:56:08 -0300

    Aplicando refatorações da aula 3 tópico 13

--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -1,28 +1,18 @@
 ﻿using Alura.Adopet.Console.Comandos;
 
-Dictionary<string, IComando> comandosDoSistema = new()
-{
-    { "help", new Help() },
-    { "import", new Import() },
-    { "list", new List() },
-    { "show", new Show() }
-};
+ComandosDoSistema comandos = new();
 
 Console.ForegroundColor = ConsoleColor.Green;
 
 try
 {
     string comando = args[0].Trim();
+    IComando? cmd = comandos[comando];
 
-    if (comandosDoSistema.ContainsKey(comando))
-    {
-        IComando? cmd = comandosDoSistema[comando];
+    if (cmd is not null)
         await cmd.ExecutarAsync(args);
-    }
     else
-    {
         Console.WriteLine("Comando inválido!");
-    }
 }
 catch (Exception ex)
 {
