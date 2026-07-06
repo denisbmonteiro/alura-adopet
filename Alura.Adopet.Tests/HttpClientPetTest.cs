437532dd84ca037b0c91e5251b2db83fe8b16587
Denis Bittencourt <denis765@gmail.com>
2026-07-05 21:27:56 -0300

    Aplicando refatorações da aula 4 tópico 5

---
+++ Alura.Adopet.Tests/HttpClientPetTest.cs
@@ -0,0 +1,20 @@
+﻿using Alura.Adopet.Console.Servicos;
+
+namespace Alura.Adopet.Tests;
+
+public class HttpClientPetTest
+{
+    [Fact]
+    public async Task ListaPetsDeveRetornarUmaListaNaoVazia()
+    {
+        //Arrange
+        var clientePet = new HttpClientPet();
+
+        //Act
+        var lista = await clientePet.ListPetsAsync();
+
+        //Assert
+        Assert.NotNull(lista);
+        Assert.NotEmpty(lista);
+    }
+}
