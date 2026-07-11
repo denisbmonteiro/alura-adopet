35102e325c8930b0858d4c6356c6ba7d4bcebf65
Denis Bittencourt <denis765@gmail.com>
2026-07-11 20:23:06 -0300

    Refatora a classe HttpClientPet para melhorar a legibilidade e a consistência do código

--- Alura.Adopet.Console/Servicos/HttpClientPet.cs
+++ Alura.Adopet.Console/Servicos/HttpClientPet.cs
@@ -6,30 +6,30 @@ namespace Alura.Adopet.Console.Servicos;
 
 public class HttpClientPet
 {
-    private HttpClient client;
+    private readonly HttpClient _client;
+
     public HttpClientPet(string uri = "http://localhost:5057")
     {
-        client = ConfiguraHttpClient(uri);
+        _client = SetupHttpClient(uri);
     }
 
-    HttpClient ConfiguraHttpClient(string url)
+    private static HttpClient SetupHttpClient(string url)
     {
-        HttpClient _client = new HttpClient();
-        _client.DefaultRequestHeaders.Accept.Clear();
-        _client.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/json"));
-        _client.BaseAddress = new Uri(url);
-        return _client;
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Accept.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.BaseAddress = new Uri(url);
+        return httpClient;
     }
 
     public Task CreatePetAsync(Pet pet)
     {
-        return client.PostAsJsonAsync("pet/add", pet);
+        return _client.PostAsJsonAsync("pet/add", pet);
     }
 
     public async Task<IEnumerable<Pet>?> ListPetsAsync()
     {
-        HttpResponseMessage response = await client.GetAsync("pet/list");
+        var response = await _client.GetAsync("pet/list");
         return await response.Content.ReadFromJsonAsync<IEnumerable<Pet>>();
     }
 }
