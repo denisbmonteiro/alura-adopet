731576848bcf46c93d98f4c5b2d28b62e45bb889
Denis Bittencourt <denis765@gmail.com>
2026-07-13 23:59:19 -0300

    Refatora a classe List para utilizar a injeção de dependência do HttpClientPet

--- Alura.Adopet.Console/Comandos/List.cs
+++ Alura.Adopet.Console/Comandos/List.cs
@@ -5,6 +5,13 @@ namespace Alura.Adopet.Console.Comandos;
 [DocComando(instrucao: "list", documentacao: "adopet list comando que exibe no terminal o conteúdo cadastrado na base de dados da AdoPet.")]
 internal class List : IComando
 {
+    private readonly HttpClientPet _httpClientPet;
+
+    public List(HttpClientPet httpClientPet)
+    {
+        _httpClientPet = httpClientPet;
+    }
+
     public Task ExecutarAsync(string[] args)
     {
         return ListaDadosPetsDaAPIAsync();
@@ -14,8 +21,7 @@ internal class List : IComando
     {
         System.Console.WriteLine("----- Lista de Pets importados no sistema -----");
 
-        var httpClientPet = new HttpClientPet();
-        var pets = await httpClientPet.ListPetsAsync();
+        var pets = await _httpClientPet.ListPetsAsync();
 
         if (pets is null)
         {
