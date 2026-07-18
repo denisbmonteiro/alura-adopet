adfa393779fbbca9878ea3f44405e123047fbbfb
Denis Bittencourt <denis765@gmail.com>
2026-07-18 04:19:05 -0300

    Refatora a interface IPetRepository para corrigir a estrutura e melhorar a legibilidade do código

--- Alura.Adopet.API/Dominio/Interface/Repository/IPetRepository.cs
+++ Alura.Adopet.API/Dominio/Interface/Repository/IPetRepository.cs
@@ -1,13 +1,12 @@
 ﻿using Alura.Adopet.API.Dominio.Entity;
 
-namespace Alura.Adopet.API.Dominio.Interface.Repository
+namespace Alura.Adopet.API.Dominio.Interface.Repository;
+
+internal interface IPetRepository
 {
-    internal interface IPetRepository
-    {
-        public Task<List<Pet>> ObterTodos();        
-        public Pet ObterPorId(Guid id);
-        public Pet Adicionar(Pet _pet);
-        public Pet Atualizar(int id, Pet _pet);
-        public bool Excluir(Guid id);
-    }
+    public Task<List<Pet>> ObterTodos();
+    public Pet ObterPorId(Guid id);
+    public Pet Adicionar(Pet _pet);
+    public Pet Atualizar(int id, Pet _pet);
+    public bool Excluir(Guid id);
 }
