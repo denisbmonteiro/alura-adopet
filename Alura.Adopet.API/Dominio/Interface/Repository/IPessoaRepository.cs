e0552f9b1df18dab1fc49ddb72a719d05e9e66bd
Denis Bittencourt <denis765@gmail.com>
2026-07-18 04:18:50 -0300

    Refatora a interface IClienteRepository para corrigir a estrutura e melhorar a legibilidade do código

--- Alura.Adopet.API/Dominio/Interface/Repository/IPessoaRepository.cs
+++ Alura.Adopet.API/Dominio/Interface/Repository/IPessoaRepository.cs
@@ -1,13 +1,12 @@
 ﻿using Alura.Adopet.API.Dominio.Entity;
 
-namespace Alura.Adopet.API.Dominio.Interface.Repository
+namespace Alura.Adopet.API.Dominio.Interface.Repository;
+
+internal interface IClienteRepository
 {
-    internal interface IClienteRepository
-    {
-        public List<Cliente> ObterTodos();
-        public Cliente ObterPorId(Guid id);
-        public Cliente Adicionar(Cliente _pessoa);
-        public Cliente Atualizar(int id, Cliente _pessoa);
-        public bool Excluir(Guid id);
-    }
+    public List<Cliente> ObterTodos();
+    public Cliente ObterPorId(Guid id);
+    public Cliente Adicionar(Cliente _pessoa);
+    public Cliente Atualizar(int id, Cliente _pessoa);
+    public bool Excluir(Guid id);
 }
