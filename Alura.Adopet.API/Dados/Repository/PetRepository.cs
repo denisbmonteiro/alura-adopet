a8fa7e0dbd64884a9ff0a152d3f4c0876b72de19
Denis Bittencourt <denis765@gmail.com>
2026-07-18 04:18:25 -0300

    Refatora a estrutura da classe PetRepository para melhorar a legibilidade e organização do código

--- Alura.Adopet.API/Dados/Repository/PetRepository.cs
+++ Alura.Adopet.API/Dados/Repository/PetRepository.cs
@@ -3,54 +3,55 @@ using Alura.Adopet.API.Dominio.Entity;
 using Alura.Adopet.API.Dominio.Interface.Repository;
 using Microsoft.EntityFrameworkCore;
 
-namespace Alura.Adopet.API.Dados.Repository
+namespace Alura.Adopet.API.Dados.Repository;
+
+internal class PetRepository : IPetRepository
 {
-    internal class PetRepository : IPetRepository
+    private DataBaseContext _context;
+
+    public PetRepository(DataBaseContext ctx)
     {
-        private DataBaseContext _context;
-        public PetRepository(DataBaseContext ctx)
-        {
-            _context= ctx;
-        }
-        public Pet Adicionar(Pet _pet)
-        {
-            _context.Add(_pet);
-            _context.SaveChanges();
-            return _pet;
-        }
+        _context= ctx;
+    }
 
-        public Pet Atualizar(int id, Pet _pet)
-        {
-            var _obj = this.ObterPorId(_pet.Id);
-            if (_obj == null)
-            {
-                return null;
-            }
-            _context.Entry(_pet).State = EntityState.Modified;
-            _context.SaveChanges();
-            return _pet;
-        }
+    public Pet Adicionar(Pet _pet)
+    {
+        _context.Add(_pet);
+        _context.SaveChanges();
+        return _pet;
+    }
 
-        public bool Excluir(Guid id)
+    public Pet Atualizar(int id, Pet _pet)
+    {
+        var _obj = this.ObterPorId(_pet.Id);
+        if (_obj == null)
         {
-            var _obj = this.ObterPorId(id);
-            if (_obj == null)
-            {
-                return false;
-            }
-            _context.Remove(_obj);
-            _context.SaveChanges();
-            return true;
+            return null;
         }
+        _context.Entry(_pet).State = EntityState.Modified;
+        _context.SaveChanges();
+        return _pet;
+    }
 
-        public Pet? ObterPorId(Guid id)
+    public bool Excluir(Guid id)
+    {
+        var _obj = this.ObterPorId(id);
+        if (_obj == null)
         {
-            return _context.Pets.Where(a => a.Id == id).FirstOrDefault();
+            return false;
         }
+        _context.Remove(_obj);
+        _context.SaveChanges();
+        return true;
+    }
 
-        public async Task<List<Pet>> ObterTodos()
-        {
-            return await _context.Pets.Include(prop=>prop.Proprietario).ToListAsync();
-        }
+    public Pet? ObterPorId(Guid id)
+    {
+        return _context.Pets.Where(a => a.Id == id).FirstOrDefault();
+    }
+
+    public async Task<List<Pet>> ObterTodos()
+    {
+        return await _context.Pets.Include(prop=>prop.Proprietario).ToListAsync();
     }
 }
